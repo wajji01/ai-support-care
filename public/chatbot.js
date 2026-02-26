f09ea8e73b4a99afddc84f02de8fec780a7a9b45
Wajahat Mustafa <125201786+wajji01@users.noreply.github.com>
2026-02-26 23:53:09 +0500

    new chancges

--- public/chatbot.js
+++ public/chatbot.js
@@ -1,5 +1,5 @@
 (function () {
-  const api_Url = "http://localhost:3000/api/chat";
+  const api_Url = "https://ai-support-care-iota.vercel.app/api/chat";
 
   const scriptTag = document.currentScript;
   const ownerId = scriptTag.getAttribute("data-owner-id");
